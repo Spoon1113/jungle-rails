d1d6199f2bfc5295f2e99aa4e5142f0fcebf131d
Samson Poon <samsonkfpoon@gmail.com>
2023-03-14 19:58:21 +0000

    add test for navigating to product details page onclick

--- cypress/integration/product_details.spec.js
+++ cypress/integration/product_details.spec.js
@@ -1,10 +1,14 @@
-describe('Product Details', () => {
+describe("Product Details", () => {
   beforeEach(() => {
-    cy.visit('/')
-  })
+    cy.visit("/");
+  });
 
   it("There is products on the page", () => {
-    cy.get(".product article").should("be.visible")
-  })    
-  
-})
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("Navigates from home page to product details page by clicking on product", () => {
+    cy.get(".products article").first().click();
+    cy.get(".product-detail").should("be.visible");
+  });
+});
